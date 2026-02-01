756811a1b6ddbd21981dad9496ae0e4c4318306f
kstetar <kstetar@gmail.com>
2026-02-01 23:32:44 +0000

    manager screen initial

--- BusShuttleProject/BusShuttle/DataManager.cs
+++ BusShuttleProject/BusShuttle/DataManager.cs
@@ -7,29 +7,50 @@ public class DataManager {
     FileSaver fileSaver;
     public DataManager() {
         fileSaver = new FileSaver("passenger-data.txt");
+        
         Loops = new List<Loop>();
         Loops.Add(new Loop("Red"));
         Loops.Add(new Loop("Green"));
         Loops.Add(new Loop("Blue"));
         Loops.Add(new Loop("Orange"));
+        
         Stops = new List<Stop>();
-        Stops.Add(new Stop("Oakwood"));
-        Stops.Add(new Stop("Anthony"));
-        Stops.Add(new Stop("North Bus Shelter"));
-        Stops.Add(new Stop("North Dining"));
-        Stops.Add(new Stop("Letterman"));
+        var stopsFileContent = File.ReadAllLines("stops.txt");
+        foreach(var stopName in stopsFileContent) {
+            Stops.Add(new Stop(stopName));
+        }
+
+        
         Loops[0].Stops.Add(Stops[0]);
         Loops[0].Stops.Add(Stops[1]);
         Loops[0].Stops.Add(Stops[2]);
         Loops[0].Stops.Add(Stops[3]);
         Loops[0].Stops.Add(Stops[4]);
+
         Drivers = new List<Driver>();
-        Drivers.Add(new Driver("Huseyin Ergin"));
-        Drivers.Add(new Driver("Jane Doe"));
+        Drivers.Add(new Driver("Usual Driver"));
+        Drivers.Add(new Driver("Alternate Driver"));
+        
         PassengerData = new List<PassengerData>();
     }
     public void AddNewPassengerData(PassengerData data) {
         this.PassengerData.Add(data);
         this.fileSaver.AppendData(data);
     }
+
+    public void SynchronizeStops() {
+        File.Delete("stops.txt");
+        foreach(var stop in Stops) {
+            File.AppendAllText("stops.txt",stop.Name+Environment.NewLine);
+        }
+    }
+    public void AddStop(string newStopName) {
+        this.Stops.Add(new Stop(newStopName));
+        this.SynchronizeStops();
+    }
+    public void RemoveStop(Stop toRemove) {
+        this.Stops.Remove(toRemove);
+        this.SynchronizeStops();
+    }
+
 }
